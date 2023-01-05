5604bb5e4ab1f1bceb1f2e7cb6017a2397a8b613
Shaurul3 <94381820+Shaurul3@users.noreply.github.com>
2023-01-05 18:04:16 +0200

    Mirror the img

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -1,22 +1,49 @@
-fetch("data.json")
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    // Extract the image URL from the JSON data
-    const imageUrl = data.message;
+var xhr = new XMLHttpRequest();
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    // Parse the response text as JSON
+    var data = JSON.parse(xhr.responseText);
 
-    // Get a reference to the canvas element
-    const canvas = document.getElementById('myCanvas');
-    const ctx = canvas.getContext('2d');
+    // Extract the image URL from the JSON data
+    var imageUrl = data.message;
 
     // Create a new image element
-    const img = new Image();
+    var img = new Image();
 
-    // Set the src property of the image element to the image URL
+    // Set the src attribute of the image element to the image URL
     img.src = imageUrl;
 
-    // When the image has finished loading, draw it onto the canvas
+    // When the image has finished loading, set the size of the canvas to the natural size of the image
     img.onload = function() {
-      ctx.drawImage(img, 0, 0);
+      var width = img.naturalWidth;
+      var height = img.naturalHeight;
+
+      // Get a reference to the canvas element
+      var canvas = document.getElementById("myCanvas");
+
+      // Set the width and height of the canvas to the natural size of the image
+      canvas.width = width;
+      canvas.height = height;
+
+      // Get a 2D context for the canvas
+      var ctx = canvas.getContext("2d");
+
+      // Save the current canvas state
+      ctx.save();
+
+      // Flip the image horizontally
+      ctx.scale(-1, 1);
+
+      // Draw the image on the canvas
+      ctx.drawImage(img, -width, 0);
+
+      // Move the image back into the visible area of the canvas
+      ctx.translate(width, 0);
+
+      // Restore the canvas state
+      ctx.restore();
     };
-  });
+  }
+};
+xhr.open("GET", "data.json", true);
+xhr.send(null);
